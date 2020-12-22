3774aeea2731b387345f5bb75f74de40be754b81
BakaVaka <hotwetloli2008@gmail.com>
2020-12-23 02:05:40 +0300

    deploy

---
+++ precache-manifest.1235b91bc0cffd52da5acf240628faef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9adf004b24e346f7c5a",
+    "url": "/css/app.d386a31b.css"
+  },
+  {
+    "revision": "63ddf3794851df4e23e32632ce255b47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42a021edd355e13dfa98",
+    "url": "/js/about.2cfa69d0.js"
+  },
+  {
+    "revision": "b9adf004b24e346f7c5a",
+    "url": "/js/app.c0055326.js"
+  },
+  {
+    "revision": "942a841bdc26234fe999",
+    "url": "/js/chunk-vendors.8380736d.js"
+  },
+  {
+    "revision": "833d8182528a24c31b14f9eb0af10af2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
